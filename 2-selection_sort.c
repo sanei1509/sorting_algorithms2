d7d5cef96bd5fe61471baef2e8c80b756ba3fcc9
MateoArbini <mateoarbini1998@gmail.com>
2022-06-08 11:43:32 -0700

    selection sort algorithm task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,32 @@
 #include "sort.h"
-
 /**
 *selection_sort - sort and array in ascending order
 *@array: array of elements
 *@size: size of array
 */
-
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i = 0, j = 0, jMin = 0;
-    int aux = 0;
+	unsigned int i = 0, j = 0, x = 0;
+	int aux = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        jMin = i;
-        for (j = i+1; j < size; j++)
-        {
-            if (array[j] < array[jMin])
-            {
-                jMin = j;
-            }
-        }
-
-        if (jMin != i) 
-        {
-            aux = array[i];
-            array[i] = array[jMin];
-            array[jMin] = aux;
-            print_array(array, size);
-        }
-    }
+	if (size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		x = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[x])
+				x = j;
+		}
+		if (x != i)
+		{
+			aux = array[i];
+			array[i] = array[x];
+			array[x] = aux;
+			print_array(array, size);
+		}
+	}
 }
