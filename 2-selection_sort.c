a848f0c05e9ac358f40d1f831cacbd925a1df3e9
Santiago Neira <4373@holbertonschool.com>
2022-06-08 18:41:46 +0000

    try sort array with selection algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,5 +8,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-    
+    unsigned int i = 0, j = 0, jMin = 0;
+    int aux = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        jMin = i;
+        for (j = i+1; j < size; j++)
+        {
+            if (array[j] < array[jMin])
+            {
+                jMin = j;
+            }
+        }
+
+        if (jMin != i) 
+        {
+            aux = array[i];
+            array[i] = array[jMin];
+            array[jMin] = aux;
+            print_array(array, size);
+        }
+    }
 }
