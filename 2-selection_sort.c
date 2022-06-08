cfc9e3f66232e8e856d88fbacb43d431383011e4
Santiago Neira <4373@holbertonschool.com>
2022-06-08 18:21:23 +0000

    working in selection algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+*selection_sort - sort and array in ascending order
+*@array: array of elements
+*@size: size of array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    
+}
