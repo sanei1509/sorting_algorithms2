7ef48305db42c3a8073a493b4aa17317da0df4bc
MateoArbini <mateoarbini1998@gmail.com>
2022-06-08 11:12:29 -0700

    insertion sort algorithm task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+/**
+ *
+ *
+ **/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL;
+	listint_t *newnode = NULL;
+
+	while (current->next != NULL)
+	{
+		if (current->n < current->next->n)
+		{
+			newnode->n = 
+			
+
+}
