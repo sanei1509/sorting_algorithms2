97ba11f42a04fb376ea6c6a1044279b6975d4e3b
MateoArbini <mateoarbini1998@gmail.com>
2022-06-09 10:59:05 -0700

    quick sort algorithm task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,4 +16,44 @@ void change_position(int *number1, int *number2)
  *
  *
  **/
+int partition(int *array, int start, int end)
+{
+	int pivot = array[end];
+	int Index = start;
+	int i = 0;
 
+	for (i = start; i < end; i++)
+	{
+		if (array[i] < pivot)
+		{
+			change_position(&array[i], &array[Index]);
+			Index++;
+		}
+	}
+	change_position(&array[end], &array[Index]);
+	return Index;
+}
+/**
+ *
+ *
+ **/
+void quickSort(int *array, int start, int end)
+{
+	int pi = 0;
+
+	if(start < end)
+	{
+		pi = partition(array, start, end);
+		quickSort(array, start, pi - 1);
+		quickSort(array, pi + 1, end);
+	}
+}
+/**
+ *
+ *
+ **/
+void quick_sort(int *array, size_t size)
+{
+	quickSort(array, 0, size - 1);
+	print_array(array, size);
+}
