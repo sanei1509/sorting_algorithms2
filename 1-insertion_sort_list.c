4c97fd60f26f39b35ae989c36ebd19dfb269f25c
Santiago Neira <4373@holbertonschool.com>
2022-06-09 16:42:36 +0000

    try task one

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,44 +9,40 @@ void insertion_sort_list(listint_t **list)
 	listint_t *p = NULL, *curr = NULL, *anterior = NULL;
 
 	/*zero or one element in list ?*/
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
-	/*no puedo pararme en el primero porque tengo
-	que evaluar el anterior nodo*/	
+	/*no puedo pararme en el primero porque tengo que evaluar el anterior nodo*/
 	curr = (*list)->next;
 	while (curr != NULL)
 	{
 		/*vamos hasta el último pero sin perder el 2do*/
 		p = curr;
 		curr = curr->next;
-		/*el actual es más chico que el anterior?*/
-		if (p->n < p->prev->n)
+		anterior = p->prev;
+		while (anterior != NULL && anterior->n > p->n)
 		{
-			anterior = p->prev;
-			/*si no estamos en el 1ero y el anterior > siguiente*/
-			/*debemos swapear*/
-			while (anterior != NULL && anterior->n > p->n)
+			anterior->next = p->next;
+			if (p->next !=  NULL)
+				p->next->prev = p->prev;
+			if (anterior->prev == NULL)/*si estamos en el nodo más chico*/
 			{
-				/*el sig */
-				anterior->next = p->next;
-				if (p->next !=  NULL)
-					p->next->prev = p->prev;
-				/*si estamos en el nodo más chico*/
-				if (anterior->prev == NULL)
-				{
-					/*el segundo pasa a ser el 1ero*/
-					p->prev = NULL;
-					p->next = anterior;
-					p->next->prev = p;
-					*list = p;
-					print_list(*list);
-				}
-				/*me paro en el anterior de ese?*/
-				anterior = anterior->prev->prev;
+				p->prev = NULL;
+				p->next = anterior;
+				p->next->prev = p;
+				*list = p; /*el segundo pasa a ser el 1ero*/
+				print_list(*list);
 			}
+			else
+			{
+				anterior->prev->next = p;
+				p->prev = anterior->prev;
+				anterior->prev = p;
+				p->next = anterior;
+				print_list(*list);
+			}
+			anterior = anterior->prev->prev;
 		}
-
 	}
 }
