b1b80313993e25d2531ba758ab91f893b974c1f2
MateoArbini <mateoarbini1998@gmail.com>
2022-06-09 11:48:13 -0700

    quick sort algortihm task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,20 +3,19 @@
  *
  *
  **/
-void change_position(int *number1, int *number2)
+void change_position(int *array, int *number1, int *number2, size_t size)
 {
-	int exchange_position = 0;
+	int exchange_position = *number1;
 
-	exchange_position = *number1;
 	*number1 = *number2;
 	*number2 = exchange_position;
+	print_array(array, size);
 }
-
 /**
  *
  *
  **/
-int partition(int *array, int start, int end)
+int partition(int *array, int start, int end, size_t size)
 {
 	int pivot = array[end];
 	int Index = start;
@@ -24,28 +23,32 @@ int partition(int *array, int start, int end)
 
 	for (i = start; i < end; i++)
 	{
-		if (array[i] < pivot)
+		if (array[i] <= pivot)
 		{
-			change_position(&array[i], &array[Index]);
+			if (Index != i)
+			{
+				change_position(array, &array[i], &array[Index], size);
+			}
 			Index++;
 		}
 	}
-	change_position(&array[end], &array[Index]);
+	if (Index != end)
+		change_position(array, &array[end], &array[Index], size);
 	return Index;
 }
 /**
  *
  *
  **/
-void quickSort(int *array, int start, int end)
+void quickSort(int *array, int low, int high, size_t size)
 {
 	int pi = 0;
 
-	if(start < end)
+	if(low < high)
 	{
-		pi = partition(array, start, end);
-		quickSort(array, start, pi - 1);
-		quickSort(array, pi + 1, end);
+		pi = partition(array, low, high, size);
+		quickSort(array, low, pi - 1, size);
+		quickSort(array, pi + 1, high, size);
 	}
 }
 /**
@@ -54,6 +57,5 @@ void quickSort(int *array, int start, int end)
  **/
 void quick_sort(int *array, size_t size)
 {
-	quickSort(array, 0, size - 1);
-	print_array(array, size);
+	quickSort(array, 0, size - 1, size);
 }
