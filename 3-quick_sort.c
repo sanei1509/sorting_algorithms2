8d0a8318f25ac8faf27c3a29b8c59842395a7b40
MateoArbini <mateoarbini1998@gmail.com>
2022-06-09 12:18:35 -0700

    quick sorting algorithm task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,10 @@
 #include "sort.h"
 /**
- *
- *
+ *change_position - funtion that changes the position of the numbers
+ *@array: array
+ *@number1: first number
+ *@number2: second number
+ *@size: size of the array
  **/
 void change_position(int *array, int *number1, int *number2, size_t size)
 {
@@ -12,16 +15,20 @@ void change_position(int *array, int *number1, int *number2, size_t size)
 	print_array(array, size);
 }
 /**
- *
- *
+ *partition - partition function for the array
+ *@array: array
+ *@low: low
+ *@high: high
+ *@size: size of the array
+ *Return: index
  **/
-int partition(int *array, int start, int end, size_t size)
+int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[end];
-	int Index = start;
+	int pivot = array[high];
+	int Index = low;
 	int i = 0;
 
-	for (i = start; i < end; i++)
+	for (i = low; i < high; i++)
 	{
 		if (array[i] <= pivot)
 		{
@@ -32,19 +39,22 @@ int partition(int *array, int start, int end, size_t size)
 			Index++;
 		}
 	}
-	if (Index != end)
-		change_position(array, &array[end], &array[Index], size);
-	return Index;
+	if (Index != high)
+		change_position(array, &array[high], &array[Index], size);
+	return (Index);
 }
 /**
- *
- *
+ *quickSort - function to quicksort
+ *@array: array
+ *@low: low
+ *@high: high
+ *@size: size of the array
  **/
 void quickSort(int *array, int low, int high, size_t size)
 {
 	int pi = 0;
 
-	if(low < high)
+	if (low < high)
 	{
 		pi = partition(array, low, high, size);
 		quickSort(array, low, pi - 1, size);
@@ -52,10 +62,13 @@ void quickSort(int *array, int low, int high, size_t size)
 	}
 }
 /**
- *
- *
+ *quick_sort - sorts an array of integers in ascending order
+ *@array: array
+ *@size: size of the array
  **/
 void quick_sort(int *array, size_t size)
 {
+	if (!array || size < 2)
+		return;
 	quickSort(array, 0, size - 1, size);
 }
