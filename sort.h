07955d3fb93d6ea927dc55cde56b66491a079131
Santiago Neira <4373@holbertonschool.com>
2022-06-11 11:48:22 +0000

    fix betty of header

--- sort.h
+++ sort.h
@@ -14,7 +14,9 @@
 typedef struct listint_s
 {
 const int n;
-struct listint_s *prev;} listint_t;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
